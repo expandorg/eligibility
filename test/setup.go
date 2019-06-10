e89a0110ca5fd6d58bc01943c24ee2c5979f56e9
Ed Shadi <edshadi@gmail.com>
2019-06-10 17:21:50 +0200

    adds mysql mock

--- test/setup.go
+++ test/setup.go
@@ -2,18 +2,13 @@ package test
 
 import (
 	"database/sql"
-	"log"
+
+	"github.com/gemsorg/eligibility/mock"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jmoiron/sqlx"
 )
 
-func Setup() (*sql.DB, *sqlx.DB, sqlmock.Sqlmock) {
-	mockDB, mock, err := sqlmock.New()
-	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
-
-	if err != nil {
-		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	return mockDB, sqlxDB, mock
+func Setup() (*sql.DB, *sqlx.DB, {sqlmock.Sqlmock}) {
+	return mock.Mysql()
 }
