b9ee55d599b2606cfc5c272ac3db4d5941786082
Ed Shadi <edshadi@gmail.com>
2019-06-17 19:55:29 +0200

    authorization package and authorize create and fetch worker profiles

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"github.com/gemsorg/eligibility/pkg/authorization"
+
 	"github.com/gemsorg/eligibility/pkg/api/workerprofilecreator"
 	"github.com/gemsorg/eligibility/pkg/authentication"
 
@@ -24,7 +26,8 @@ import (
 func New(db *sqlx.DB) http.Handler {
 	r := mux.NewRouter()
 	ds := datastore.NewEligibilityStore(db)
-	s := service.New(ds)
+	authorizer := authorization.NewAuthorizer()
+	s := service.New(ds, authorizer)
 	r.Handle("/_health", healthchecker.MakeHandler(s)).Methods("GET")
 	r.Handle("/filters", filtersfetcher.MakeHandler(s)).Methods("GET")
 	r.Handle("/filters", filtercreator.MakeHandler(s)).Methods("POST")
